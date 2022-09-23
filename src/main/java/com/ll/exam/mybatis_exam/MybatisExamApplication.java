756d7b10a3025dc52060b5ba955d8a2d0c8c6920
EunJung803 <aimee3322@gmail.com>
2022-09-23 09:25:14 +0900

    Setting

---
+++ src/main/java/com/ll/exam/mybatis_exam/MybatisExamApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.mybatis_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/exam/mybatis_exam/MybatisExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.mybatis_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
