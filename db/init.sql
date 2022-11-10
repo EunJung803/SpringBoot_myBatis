a44dc28df48df461d7bc8e17aaad4db81c2b767a
EunJung803 <aimee3322@gmail.com>
2022-11-10 16:06:20 +0900

    Feat : 회원 스키마 추가

--- db/init.sql
+++ db/init.sql
@@ -2,6 +2,7 @@ DROP DATABASE IF EXISTS mybatis_exam;
 CREATE DATABASE mybatis_exam;
 USE mybatis_exam;
 
+# 게시글 테이블 생성
 CREATE TABLE article (
      id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
      PRIMARY KEY(id),
@@ -11,6 +12,7 @@ CREATE TABLE article (
      content LONGTEXT NOT NULL
 );
 
+# 게시글 데이터 생성
 INSERT INTO article
 SET createDate = NOW(),
 modifyDate = NOW(),
@@ -22,3 +24,32 @@ SET createDate = NOW(),
 modifyDate = NOW(),
 `subject` = '제목2',
 content = '내용2';
+
+# 회원 테이블 생성
+CREATE TABLE `member` (
+      id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+      PRIMARY KEY(id),
+      createDate DATETIME NOT NULL,
+      modifyDate DATETIME NOT NULL,
+      username CHAR(100) NOT NULL UNIQUE,
+      `password` CHAR(100) NOT NULL,
+      `name` CHAR(100) NOT NULL,
+      email CHAR(100) NOT NULL
+);
+
+# 회원 데이터 생성
+INSERT INTO `member`
+SET createDate = NOW(),
+    modifyDate = NOW(),
+    username = 'user1',
+    `password` = '{noop}1234',
+    `name` = '유저1',
+    email = 'user1@test.com';
+
+INSERT INTO `member`
+SET createDate = NOW(),
+    modifyDate = NOW(),
+    username = 'user2',
+    `password` = '{noop}1234',
+    `name` = '유저2',
+    email = 'user2@test.com';
