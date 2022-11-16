62d271b8c7a19ddca34f2b64ba0bbfabb803e4c6
EunJung803 <aimee3322@gmail.com>
2022-11-16 16:44:58 +0900

    Feat : rq 빈을 세션 스코프로 생성

--- src/main/java/com/ll/exam/mybatis_exam/app/base/rq/Rq.java
+++ src/main/java/com/ll/exam/mybatis_exam/app/base/rq/Rq.java
@@ -3,8 +3,11 @@ package com.ll.exam.mybatis_exam.app.base.rq;
 import lombok.Getter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.context.annotation.SessionScope;
 
 @Component
+//@ApplicationScope   // 스프링부트 앱 당 1개
+@SessionScope       // 브라우저 당 1개
 public class Rq {
     @Getter
     private int count;
