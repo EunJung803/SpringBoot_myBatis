b5f1343574a95e47da905eab7af020e3c2c42eb1
EunJung803 <aimee3322@gmail.com>
2022-12-02 23:19:01 +0900

    Feat : rq 를 리퀘스트 스코프로 변경 해당 객체는 하나의 요청 안에서 인터셉터, 컨트롤러, 템플릿끼리 공유할 데이터를 담을 때 유용함

--- src/main/java/com/ll/exam/mybatis_exam/app/base/rq/Rq.java
+++ src/main/java/com/ll/exam/mybatis_exam/app/base/rq/Rq.java
@@ -1,22 +1,93 @@
 package com.ll.exam.mybatis_exam.app.base.rq;
 
+import com.ll.exam.mybatis_exam.app.member.dto.Member;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.context.annotation.RequestScope;
 import org.springframework.web.context.annotation.SessionScope;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Component
 //@ApplicationScope   // 스프링부트 앱 당 1개
-@SessionScope       // 브라우저 당 1개
+//@SessionScope       // 브라우저 당 1개
+@RequestScope
+@RequiredArgsConstructor
 public class Rq {
-    @Getter
-    private int count;
-    @Getter
-    @Setter
-    private String name;
+    private final HttpServletRequest req;
+    private final HttpServletResponse resp;
+    private final HttpSession session;
 
-    public void increaseCount() {
-        count++;
+    public String getCurrentUrl() {
+        String url = req.getRequestURL().toString();
+        String queryString = req.getQueryString();
+
+        if (queryString != null && queryString.length() > 0) {
+            url += "?" + queryString;
+        }
+
+        return url;
+    }
+
+    public boolean isLogined() {
+        return getLoginedMemberId() > 0;
+    }
+
+    public boolean isLogout() {
+        return isLogined() == false;
+    }
+
+    public long getLoginedMemberId() {
+        Long loginedMemberId = (Long) session.getAttribute("loginedMemberId");
+
+        if (loginedMemberId == null) return 0;
+
+        return loginedMemberId;
+    }
+
+    public String getLoginedMemberUsername() {
+        return (String) session.getAttribute("loginedMemberUsername");
+    }
+
+    public String getLoginedMemberName() {
+        return (String) session.getAttribute("loginedMemberName");
+    }
+
+    public String getLoginedMemberEmail() {
+        return (String) session.getAttribute("loginedMemberEmail");
+    }
+
+    public Member getLoginedMember() {
+        long id = getLoginedMemberId();
+        String username = getLoginedMemberUsername();
+        String name = getLoginedMemberName();
+        String email = getLoginedMemberEmail();
+
+        return Member
+                .builder()
+                .id(id)
+                .username(username)
+                .name(name)
+                .email(email)
+                .build();
+    }
+
+    public void setLoginDone(Member member) {
+        session.setAttribute("loginedMemberId", member.getId());
+        session.setAttribute("loginedMemberUsername", member.getUsername());
+        session.setAttribute("loginedMemberName", member.getName());
+        session.setAttribute("loginedMemberEmail", member.getEmail());
+    }
+
+    public void setLogoutDone() {
+        session.removeAttribute("loginedMemberId");
+        session.removeAttribute("loginedMemberUsername");
+        session.removeAttribute("loginedMemberName");
+        session.removeAttribute("loginedMemberEmail");
     }
 }
